5fe18e10e2ac69dc17dbef17ccc8eaba6111d482
Peter Jose <peterkjose@gmail.com>
2020-12-30 16:41:43 +0100

    Circular Buffer module (work in progress) for IPC communication

---
+++ src/WeCare_iBoard/CircularBuffer.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file CircularBuffer.cpp
+ * @author Team Hugo
+ * @brief 
+ * @version 0.1
+ * @date 2020-12-30
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+
+/* EOF */
---
+++ src/WeCare_iBoard/CircularBuffer.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file CircularBuffer.hpp
+ * @author Team Hugo
+ * @brief 
+ * @version 0.1
+ * @date 2020-12-30
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#ifndef _CIRCULAR_BUFFER_H_
+#define _CIRCULAR_BUFFER_H_
+
+
+
+#endif /* _CIRCULAR_BUFFER_H_ */
