122ae1efc7867423647149b45d86063a98f0006f
Peter Jose <peterkjose@gmail.com>
2021-01-01 22:34:15 +0100

    Circular Buffer - For IPC communcation

--- src/WeCare_iBoard/CircularBuffer.cpp
+++ src/WeCare_iBoard/CircularBuffer.cpp
@@ -9,5 +9,112 @@
  * 
  */
 
+#include "CircularBuffer.hpp"
+
+/**
+ * @brief 
+ * 
+ * @param val 
+ * @return true 
+ * @return false 
+ */
+bool CircularBuffer_t::pushByte(uint8_t val)
+{
+    if (this->getByteCount() != this->maxSize)
+    {
+        this->buffer[head] = val;
+        head = (head + 1) % this->maxSize;
+        return true;
+    }
+    return false;
+}
+
+/**
+ * @brief 
+ * 
+ * @param elmArr 
+ * @param elmCount 
+ * @return true 
+ * @return false 
+ */
+bool CircularBuffer_t::pushBytes(uint8_t *elmArr,int elmCount)
+{
+    if (this->maxSize - this->getByteCount() >= elmCount)
+    {
+        for(int i =0; i < elmCount;i++)
+        {
+            this->buffer[this->head] = elmArr[i];
+            this->head = (this->head + 1) % this->maxSize;
+        }
+        return true;
+    }
+    return false;
+}
+
+/**
+ * @brief 
+ * 
+ * @param elmArr 
+ * @param elmCount 
+ * @return true 
+ * @return false 
+ */
+bool CircularBuffer_t::popBytes(uint8_t *elmArr,int elmCount)
+{
+    if (this->getByteCount() >= elmCount)
+    {
+        for(int i =0; i < elmCount;i++)
+        {
+            elmArr[i] = this->buffer[this->tail];
+            this->tail = (this->tail + 1) % this->maxSize;
+        }
+        return true;
+    }
+    return false;
+}
+
+/**
+ * @brief 
+ * 
+ * @param elmArr 
+ * @param elmCount 
+ * @return true 
+ * @return false 
+ */
+bool CircularBuffer_t::peekBytes(uint8_t *elmArr,int elmCount)
+{
+    int tailLoc = this->tail;
+    if (this->getByteCount() >= elmCount)
+    {
+        for(int i =0; i < elmCount;i++)
+        {
+            elmArr[i] = this->buffer[tailLoc];
+            tailLoc = (tailLoc + 1) % this->maxSize;
+        }
+        return true;
+    }
+    return false;
+}
+
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+inline int CircularBuffer_t::getByteCount(void)
+{
+    return ((this->head >= this->tail) ? (this->head - this->tail) : ((CIRCULAR_BUFFER_MAX_SIZE - this->tail) + this->head + 1));
+}
+
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+inline int CircularBuffer_t::getMaxBufferSize(void)
+{
+    return this->maxSize;
+}
+
 
 /* EOF */
--- src/WeCare_iBoard/CircularBuffer.hpp
+++ src/WeCare_iBoard/CircularBuffer.hpp
@@ -11,6 +11,34 @@
 #ifndef _CIRCULAR_BUFFER_H_
 #define _CIRCULAR_BUFFER_H_
 
+#include "Arduino.h"
 
+#define CIRCULAR_BUFFER_MAX_SIZE            255
+
+/**
+ * @brief 
+ * 
+ */
+typedef class CircularBuffer
+{
+    uint8_t *buffer;
+    int head;
+    int tail;
+    int maxSize; 
+public:
+    CircularBuffer(int bufferSize)
+    {
+        this->head = 0;
+        this->tail = 0;
+        this->maxSize = bufferSize;
+        buffer = (uint8_t *)malloc(bufferSize);
+    }
+    bool pushByte(uint8_t val);
+    bool pushBytes(uint8_t *elmArr,int elmCount);
+    bool popBytes(uint8_t *elmStr,int elmCount);
+    bool peekBytes(uint8_t *elmStr,int elmCount);
+    int getByteCount(void);
+    int getMaxBufferSize(void);
+} CircularBuffer_t;
 
 #endif /* _CIRCULAR_BUFFER_H_ */
