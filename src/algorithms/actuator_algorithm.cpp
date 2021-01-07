21fef5c6d3528ee546c5804afb7696066f8b0ed6
shiv-skumar <50177595+shiv-skumar@users.noreply.github.com>
2021-01-07 23:21:57 +0100

    Update actuator_algorithm.cpp
    New scenarios added

--- src/algorithms/actuator_algorithm.cpp
+++ src/algorithms/actuator_algorithm.cpp
@@ -27,3 +27,73 @@
 - When the timer runs out after a placement, it is assumed that the user is done with placement
 - The iBoard2 is actuated in a synchronous motion(up and down) until the user places a cube on any of the immobile cubes
 */
+
+//Actuation for playing games
+/*
+- Pattern to  be recognised :
+  - to activate play mode:
+    X-o
+    X-X
+    X-o
+      - No other cubes should be placed on the iBoard
+    - Change LED to green
+    - Sync actuation on iBoard2
+    - Check for iBoard2 to accept
+      - if user places a cube on an immobile cube, 
+        - stop actuation
+        - change to play mode
+  - to deactivate play mode:
+    X-X
+    X-X
+      - No other cubes should be placed on the iBoard
+    - Change LED to red
+    - Sync actuation on iBoard2
+    - Check for iBoard2 to accept
+      - if user places a cube on an immobile cube, 
+        - stop actuation
+        - change to default mode
+- Play mode:
+  - Scenario 1 and 2 used during the play
+  - Different LED colour used for each user
+  - Patter recognition:
+    - Pattern 1:
+      X-X-X-X
+    - Pattern 2:
+      X-o-o-o
+      o-X-o-o
+      o-o-X-o
+      o-o-o-X
+    - Pattern 3:
+      X-o-o-o
+      X-o-o-o
+      X-o-o-o
+      X-o-o-o
+  - If pattern recognised, sync actuation of the 'X' blocks indicating winner until the user removes a placed cube or places a cube on a immobile cube.
+*/
+
+//LED colour change for activated cubes for certain default emojis
+/* Scenario 4: Heart
+- Pattern to  be recognised :
+X-o-X
+X-X-X
+o-X-o
+- Change LED to Red
+*/
+
+/* Scenario 5: Smile
+- Pattern to  be recognised :
+o-X-X-o
+o-o-o-o
+X-o-o-X
+o-X-X-o
+- Change LED to Green/Yellow
+*/
+
+/* Scenario 6: Sad face
+- Pattern to  be recognised :
+o-X-X-o
+o-o-o-o
+o-X-X-o
+X-o-o-X
+- Change LED to Red
+*/
