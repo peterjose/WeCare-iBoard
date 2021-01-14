0dac58a6c7d9c2696e8836507b9c785ac0e93695
Peter Jose <peterkjose@gmail.com>
2021-01-14 14:57:04 +0100

    Updated Circular buffer to fix the issues with the template model code

--- src/WeCare_iBoard/CircularBuffer.cpp
+++ src/WeCare_iBoard/CircularBuffer.cpp
@@ -11,116 +11,116 @@
 
 #include "CircularBuffer.hpp"
 
-/**
- * @brief 
- * 
- * @param val 
- * @return true 
- * @return false 
- */
-template<typename T>
-bool CircularBuffer_t<T>::pushByte(T val)
-{
-    if (this->getByteCount() != this->maxSize)
-    {
-        this->buffer[head] = val;
-        head = (head + 1) % this->maxSize;
-        return true;
-    }
-    return false;
-}
+// /**
+//  * @brief 
+//  * 
+//  * @param val 
+//  * @return true 
+//  * @return false 
+//  */
+// template<typename T>
+// bool CircularBuffer_t<T>::pushByte(T val) 
+// {
+//     if (this->getByteCount() != this->maxSize)
+//     {
+//         this->buffer[head] = val;
+//         head = (head + 1) % this->maxSize;
+//         return true;
+//     }
+//     return false;
+// }
 
-/**
- * @brief 
- * 
- * @param elmArr 
- * @param elmCount 
- * @return true 
- * @return false 
- */
-template<typename T>
-bool CircularBuffer_t<T>::pushBytes(T *elmArr,int elmCount)
-{
-    if (this->maxSize - this->getByteCount() >= elmCount)
-    {
-        for(int i =0; i < elmCount;i++)
-        {
-            this->buffer[this->head] = elmArr[i];
-            this->head = (this->head + 1) % this->maxSize;
-        }
-        return true;
-    }
-    return false;
-}
+// /**
+//  * @brief 
+//  * 
+//  * @param elmArr 
+//  * @param elmCount 
+//  * @return true 
+//  * @return false 
+//  */
+// template<typename T>
+// bool CircularBuffer_t<T>::pushBytes(T *elmArr,int elmCount)
+// {
+//     if (this->maxSize - this->getByteCount() >= elmCount)
+//     {
+//         for(int i =0; i < elmCount;i++)
+//         {
+//             this->buffer[this->head] = elmArr[i];
+//             this->head = (this->head + 1) % this->maxSize;
+//         }
+//         return true;
+//     }
+//     return false;
+// }
 
-/**
- * @brief 
- * 
- * @param elmArr 
- * @param elmCount 
- * @return true 
- * @return false 
- */
-template<typename T>
-bool CircularBuffer_t<T>::popBytes(T *elmArr,int elmCount)
-{
-    if (this->getByteCount() >= elmCount)
-    {
-        for(int i =0; i < elmCount;i++)
-        {
-            elmArr[i] = this->buffer[this->tail];
-            this->tail = (this->tail + 1) % this->maxSize;
-        }
-        return true;
-    }
-    return false;
-}
+// /**
+//  * @brief 
+//  * 
+//  * @param elmArr 
+//  * @param elmCount 
+//  * @return true 
+//  * @return false 
+//  */
+// template<typename T>
+// bool CircularBuffer_t<T>::popBytes(T *elmArr,int elmCount)
+// {
+//     if (this->getByteCount() >= elmCount)
+//     {
+//         for(int i =0; i < elmCount;i++)
+//         {
+//             elmArr[i] = this->buffer[this->tail];
+//             this->tail = (this->tail + 1) % this->maxSize;
+//         }
+//         return true;
+//     }
+//     return false;
+// }
 
-/**
- * @brief 
- * 
- * @param elmArr 
- * @param elmCount 
- * @return true 
- * @return false 
- */
-template<typename T>
-bool CircularBuffer_t<T>::peekBytes(T *elmArr,int elmCount)
-{
-    int tailLoc = this->tail;
-    if (this->getByteCount() >= elmCount)
-    {
-        for(int i =0; i < elmCount;i++)
-        {
-            elmArr[i] = this->buffer[tailLoc];
-            tailLoc = (tailLoc + 1) % this->maxSize;
-        }
-        return true;
-    }
-    return false;
-}
+// /**
+//  * @brief 
+//  * 
+//  * @param elmArr 
+//  * @param elmCount 
+//  * @return true 
+//  * @return false 
+//  */
+// template<typename T>
+// bool CircularBuffer_t<T>::peekBytes(T *elmArr,int elmCount)
+// {
+//     int tailLoc = this->tail;
+//     if (this->getByteCount() >= elmCount)
+//     {
+//         for(int i =0; i < elmCount;i++)
+//         {
+//             elmArr[i] = this->buffer[tailLoc];
+//             tailLoc = (tailLoc + 1) % this->maxSize;
+//         }
+//         return true;
+//     }
+//     return false;
+// }
 
-/**
- * @brief 
- * 
- * @return int 
- */
-template<typename T>
-inline int CircularBuffer_t<T>::getByteCount(void)
-{
-    return ((this->head >= this->tail) ? (this->head - this->tail) : ((CIRCULAR_BUFFER_MAX_SIZE - this->tail) + this->head + 1));
-}
+// /**
+//  * @brief 
+//  * 
+//  * @return int 
+//  */
+// template<typename T>
+// inline int CircularBuffer_t<T>::getByteCount(void)
+// {
+//     return ((this->head >= this->tail) ? (this->head - this->tail) : ((CIRCULAR_BUFFER_MAX_SIZE - this->tail) + this->head + 1));
+// }
 
-/**
- * @brief 
- * 
- * @return int 
- */
-template<typename T>
-inline int CircularBuffer_t<T>::getMaxBufferSize(void)
-{
-    return this->maxSize;
-}
+// /**
+//  * @brief 
+//  * 
+//  * @return int 
+//  */
+// template<typename T>
+// inline int CircularBuffer_t<T>::getMaxBufferSize(void)
+// {
+//     return this->maxSize;
+// }
 
 
 /* EOF */
--- src/WeCare_iBoard/CircularBuffer.hpp
+++ src/WeCare_iBoard/CircularBuffer.hpp
@@ -15,6 +15,7 @@
 
 #define CIRCULAR_BUFFER_MAX_SIZE            255
 
+
 /**
  * @brief 
  * 
@@ -45,13 +46,132 @@ public:
     ~CircularBuffer_t(){
         free(buffer);
     }
+
     // Memeber functions
-    bool pushByte(T val);
-    bool pushBytes(T *elmArr,int elmCount);
-    bool popBytes(T *elmStr,int elmCount);
-    bool peekBytes(T *elmStr,int elmCount);
-    int getByteCount(void);
-    int getMaxBufferSize(void);
+    /**
+     * @brief 
+     * 
+     * @param val 
+     * @return true 
+     * @return false 
+     */
+    bool pushByte(T val) 
+    {
+        if (this->getByteCount() != this->maxSize)
+        {
+            this->buffer[head] = val;
+            head = (head + 1) % this->maxSize;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @brief 
+     * 
+     * @param elmArr 
+     * @param elmCount 
+     * @return true 
+     * @return false 
+     */
+    bool pushBytes(T *elmArr,int elmCount)
+    {
+        if (this->maxSize - this->getByteCount() >= elmCount)
+        {
+            for(int i =0; i < elmCount;i++)
+            {
+                this->buffer[this->head] = elmArr[i];
+                this->head = (this->head + 1) % this->maxSize;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @brief 
+     * 
+     * @param elmArr 
+     * @param elmCount 
+     * @return true 
+     * @return false 
+     */
+    bool popByte(T *element)
+    {
+        if (this->getByteCount() != 0)
+        {
+            element = this->buffer[this->tail];
+            this->tail = (this->tail + 1) % this->maxSize;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @brief 
+     * 
+     * @param elmArr 
+     * @param elmCount 
+     * @return true 
+     * @return false 
+     */
+    bool popBytes(T *elmArr,int elmCount)
+    {
+        if (this->getByteCount() >= elmCount)
+        {
+            for(int i =0; i < elmCount;i++)
+            {
+                elmArr[i] = this->buffer[this->tail];
+                this->tail = (this->tail + 1) % this->maxSize;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @brief 
+     * 
+     * @param elmArr 
+     * @param elmCount 
+     * @return true 
+     * @return false 
+     */
+    bool peekBytes(T *elmArr,int elmCount)
+    {
+        int tailLoc = this->tail;
+        if (this->getByteCount() >= elmCount)
+        {
+            for(int i =0; i < elmCount;i++)
+            {
+                elmArr[i] = this->buffer[tailLoc];
+                tailLoc = (tailLoc + 1) % this->maxSize;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @brief Get the Byte Count object
+     * 
+     * @return int 
+     */
+    int getByteCount(void)
+    {
+        return ((this->head >= this->tail) ? (this->head - this->tail) : ((CIRCULAR_BUFFER_MAX_SIZE - this->tail) + this->head + 1));
+    }
+
+    /**
+     * @brief Get the Max Buffer Size object
+     * 
+     * @return int 
+     */
+    int getMaxBufferSize(void)
+    {
+        return this->maxSize;
+    }
+
 };
 
 #endif /* _CIRCULAR_BUFFER_H_ */
