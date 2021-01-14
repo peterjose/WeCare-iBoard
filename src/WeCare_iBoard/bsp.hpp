565f78266761760950bca5e1c0093c917656df7d
maryaammm <67693648+maryaammm@users.noreply.github.com>
2021-01-14 11:29:13 +0100

    Arduino mega pins assigned.

--- src/WeCare_iBoard/bsp.hpp
+++ src/WeCare_iBoard/bsp.hpp
@@ -12,6 +12,48 @@
 #ifndef _BSP_H_
 #define _BSP_H_
 
-#define LED_SINGLE_WIRE_PIN             10
+#define LED_SINGLE_WIRE_PIN               10
+// Serial communication
+#define DEBUG_RX                          0
+#define DEBUG_TX                          1
+#define NET_TX                            18
+#define NET_RX                            19
+// Switches
+#define SWITCH01                          9
+#define SWITCH02                          11
+#define SWITCH03                          12
+#define SWITCH04                          13
+#define SWITCH05                          22
+#define SWITCH06                          23
+#define SWITCH07                          24
+#define SWITCH08                          25
+#define SWITCH09                          26
+#define SWITCH10                          27
+#define SWITCH11                          28
+#define SWITCH12                          29
+#define SWITCH13                          30
+#define SWITCH14                          31
+#define SWITCH15                          32
+#define SWITCH16                          33
+// Motors
+#define MOTOR01                           34
+#define MOTOR02                           35
+#define MOTOR03                           36
+#define MOTOR04                           37
+#define MOTOR05                           38
+#define MOTOR06                           39
+#define MOTOR07                           40
+#define MOTOR08                           41
+#define MOTOR09                           42
+#define MOTOR10                           43
+#define MOTOR11                           44
+#define MOTOR12                           45
+#define MOTOR13                           46
+#define MOTOR14                           47
+#define MOTOR15                           48
+#define MOTOR16                           49
+
+// Free digital pins: 21,20 (I2C) 19~14 (UART) 8~2 (PWM)
+// Free analog pins: A0~A15
 
 #endif /* _BSP_H_ */
