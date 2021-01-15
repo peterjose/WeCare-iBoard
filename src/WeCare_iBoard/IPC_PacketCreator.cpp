29b74d7cdf0a7ed621f821409c4ff946857b2dc1
Peter Jose <peterkjose@gmail.com>
2021-01-15 19:30:21 +0100

    IPC RX packet parser has been implemented

--- src/WeCare_iBoard/IPC_PacketCreator.cpp
+++ src/WeCare_iBoard/IPC_PacketCreator.cpp
@@ -10,15 +10,21 @@
  */
 
 // comment the following line to disable the Debug printing over the UART
-// #define DEBUG_ENABLE
+#define DEBUG_ENABLE
 #include "DebugPrint.hpp"
 
 #include "CubeModule.hpp"
 #include "IPC_PacketCreator.hpp"
+#include "IPC_communication.hpp"
 #include "MessageCreator.hpp"
+#include "TimingConfig.hpp"
+#include "SoftTimer.hpp"
 
 IPC_Packet_t IPC_packetOutgoing;
 uint8_t *OutgoingMsg;
+int headerSizeLen;
+uint16_t corruptedByteCount = 0;
+bool Incomming_IPC_packetFlag  = false;
 
 /**
  * @brief 
@@ -53,7 +59,10 @@ bool InitialiseIPC_PacketCreator(int pixelCount)
     IPC_packetOutgoing.HeaderByte2 = IPC_HEADER_BYTE2;
     IPC_packetOutgoing.HeaderByte3 = IPC_HEADER_BYTE3;
     IPC_packetOutgoing.packetNumber = INITIAL_PACKET_NO;
+    headerSizeLen = ((&(IPC_packetOutgoing.payloadByteCount) - &(IPC_packetOutgoing.HeaderByte1))
+                            /(sizeof(uint8_t)))+1;
     OutgoingMsg = (uint8_t *)malloc(GetMessageLength());
+    SetSoftTimer(IPC_PACKET_TIMEOUT_TIMER,IPC_PACKET_TIMEOUT_PERIOD);
     if(OutgoingMsg != NULL)
     {
         DBG_PRINT_LN(F("InitialiseIPC_PacketCreator >> Initialised"));
@@ -78,7 +87,7 @@ static void printCreatedMsg()
 {
     for (size_t i = 0; i < GetMessageLength(); i++)
     {
-        DBG_WRITE(OutgoingMsg[i]);
+        DBG_PRINT_HEX(OutgoingMsg[i]);
     }
     
 }
@@ -127,7 +136,61 @@ void GetOutgoingPacketCreated(IPC_Packet_t** IPC_packet_ref)
  */
 void PacketParser(CubeModule_t interactiveBoard[], int pixelCount)
 {
-
+    if(!Incomming_IPC_packetFlag)
+    {
+        SetSoftTimer(IPC_PACKET_TIMEOUT_TIMER,IPC_PACKET_TIMEOUT_PERIOD);
+    }
+    IPC_Packet_t IPC_RX_packet;
+    if(GetIPC_RX_ByteCount() > headerSizeLen)
+    {
+        Incomming_IPC_packetFlag = true;
+        if(GetIPC_RX_HeaderBytes(&IPC_RX_packet))
+        {
+            if((IPC_RX_packet.HeaderByte1 == IPC_HEADER_BYTE1) &&
+                (IPC_RX_packet.HeaderByte2 == IPC_HEADER_BYTE2) &&
+                (IPC_RX_packet.HeaderByte3 == IPC_HEADER_BYTE3))
+            {
+                if(!GetSoftTimer(IPC_PACKET_TIMEOUT_TIMER) && 
+                GetIPC_RX_ByteCount() < (headerSizeLen + IPC_RX_packet.payloadByteCount +1) )
+                {
+                    corruptedByteCount++;
+                    RemoveIPC_RX_Bytes(1);
+                    Incomming_IPC_packetFlag = false;
+                    DBG_PRINT_LN(F("PacketParser >> Pkt time out"));
+                    return;
+                }
+                if(GetIPC_RX_ByteCount() >= (headerSizeLen + IPC_RX_packet.payloadByteCount +1)){
+                    IPC_RX_packet.payload = NULL;
+                    if(GetIPC_RX_PacketBytes(&IPC_RX_packet))
+                    {
+                        if(getCRC(IPC_RX_packet.payload,IPC_RX_packet.payloadByteCount) == IPC_RX_packet.payload_CRC)
+                        {
+                            ParseMessage(interactiveBoard,pixelCount,IPC_RX_packet.payload,IPC_RX_packet.payloadByteCount);
+                            DBG_PRINT(F("PacketParser >> PKT parsed is "));
+                            DBG_PRINT_LN(IPC_RX_packet.packetNumber);
+                        }
+                        else
+                        {
+                            DBG_PRINT_LN(F("PacketParser >> CRC error"));
+                        }
+                        
+                    }
+                    if(IPC_RX_packet.payload != NULL)
+                    {
+                        free(IPC_RX_packet.payload);
+                    }
+                    Incomming_IPC_packetFlag = false;
+                }
+            }
+            else
+            {
+                corruptedByteCount++;
+                RemoveIPC_RX_Bytes(1);
+                Incomming_IPC_packetFlag = false;
+                DBG_PRINT_LN(F("PacketParser >> Pkt Corrupted"));
+            }       
+        }  
+    }
 }
 
 
