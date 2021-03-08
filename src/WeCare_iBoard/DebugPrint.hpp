d0762a9e31d2291e2fe29217c4eaa45fd6ab9cf0
Peter Jose <peterkjose@gmail.com>
2021-03-08 19:23:15 +0100

    Added a Main control for the debug With additional macro

--- src/WeCare_iBoard/DebugPrint.hpp
+++ src/WeCare_iBoard/DebugPrint.hpp
@@ -13,18 +13,29 @@
 #ifndef _DEBUG_PRINT_H_
 #define _DEBUG_PRINT_H_
 
-#ifdef DEBUG_ENABLE
-    #define DBG_BEGIN(x)        Serial.begin(x);
-    #define DBG_PRINT(x)        Serial.print(x);
-    #define DBG_PRINT_LN(x)     Serial.println(x);
-    #define DBG_PRINT_HEX(x)    Serial.print(x,HEX);
-    #define DBG_WRITE(x)        Serial.write(x);
+// To disable any Debug comment the following line 
+// #define DEBUG_ALLOWED
+
+#ifdef DEBUG_ALLOWED
+    #ifdef DEBUG_ENABLE
+        #define DBG_BEGIN(x)        Serial.begin(x);
+        #define DBG_PRINT(x)        Serial.print(x);
+        #define DBG_PRINT_LN(x)     Serial.println(x);
+        #define DBG_PRINT_HEX(x)    Serial.print(x,HEX);
+        #define DBG_WRITE(x)        Serial.write(x);
+    #else
+        #define DBG_BEGIN(x)
+        #define DBG_PRINT(x)
+        #define DBG_PRINT_LN(x)
+        #define DBG_PRINT_HEX(x)
+        #define DBG_WRITE(x)
+    #endif /* DEBUG_ENABLE */
 #else
     #define DBG_BEGIN(x)
     #define DBG_PRINT(x)
     #define DBG_PRINT_LN(x)
     #define DBG_PRINT_HEX(x)
     #define DBG_WRITE(x)
-#endif /* DEBUG_ENABLE */
+#endif /* DEBUG_ALLOWED */
 
 #endif /* _DEBUG_PRINT_H_ */
